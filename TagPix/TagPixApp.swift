7878079a67d6c947e6dbaf466199f66f732a3583
Matthew M. Burke <matthew@bluedino.net>
2024-10-11 14:40:37 -0400

    Initial Commit

---
+++ TagPix/TagPixApp.swift
@@ -0,0 +1,17 @@
+//
+//  TagPixApp.swift
+//  TagPix
+//
+//  Created by Matthew Burke on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TagPixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TagPixTests/TagPixTests.swift
@@ -0,0 +1,17 @@
+//
+//  TagPixTests.swift
+//  TagPixTests
+//
+//  Created by Matthew Burke on 10/11/24.
+//
+
+import Testing
+@testable import TagPix
+
+struct TagPixTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
